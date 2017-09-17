609c97fcbcac71ec2a09660ffa618230e1ad0de7
Alexander Foster <afoster009@hotmail.ca>
2017-09-17 19:21:18 +0000

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,18 @@
 require_relative "./vehicle.rb"
+
+# notice we a requiring our vehicle class in this file.
+# that is because our Car class will need access to the Vehicle class
+# and therefore will need access to the file that contains that class.
+
+class Car < Vehicle
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+end
+
+# We use the < to inherit the Car class from vehicle. Notice there are not methods defined
+# In the Car class.
+
+# Without writing anything in our Car class we still inherit all the methods found in
+# the Vehicle class. The only test failing in the #go method in the car class which
+# we over wrote in the car class above to display the proper return value.
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,24 @@
+# In this domain model we have a vehicle class that will act as the parent, or super class
+# We will create child classes, also know as subclasses for different types of vehicles.
+
+# First we define the Vehicle claass that will act as the parent.
 class Vehicle
+  attr_accessor :wheel_size, :wheel_number
+
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+
+  def go
+    "vrrrrrrrooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
 
 end
+
+# Instances of the vehicle class initialize with a wheel size and number.
+# we also have a #go and #fill_up_tank instance methods that describe common Vehicle behaviors.
